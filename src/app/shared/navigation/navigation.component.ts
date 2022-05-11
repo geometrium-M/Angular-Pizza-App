95b010b98a989b777604f0e0782dccafd6bbf1a0
Mindrescu Marina <mindrescum2011c@gmail.com>
2022-05-11 17:17:18 +0300

    contacts component modified

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -9,12 +9,38 @@ export class NavigationComponent  {
 
   imgLogo: string = 'assets/img/logo.svg';
 
+  
+
+  
+
   constructor() {}
+
+  
 
   public toggleMobileMenu(): any {
     document.querySelector('#burger')?.classList.toggle('active');
-    document.querySelector('#menu')?.classList.toggle('active');
-    
+    document.querySelector('#menu')?.classList.toggle('active'); 
   }
 
+  public closeMobileMenu(): any {
+    if (document.querySelector('#menu')?.classList.contains('active')) {
+      document.querySelector('#menu')?.classList.remove('active');
+      document.querySelector('#burger')?.classList.remove('active');
+    } 
+  } 
+
+  
+  
+  
+  
+    
+     
+      
+      
 }
+  
+
+
+
+
+
